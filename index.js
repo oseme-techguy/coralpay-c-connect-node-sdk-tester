e085d911c43ebb79ac860df3cf322f7d11b867b6
Oseme <osemeodigie@Osemes-MacBook-Pro.local>
2020-06-02 14:32:35 +0100

    feat: updated the tester script to reflect updates to the JS library

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const { CoralPayCConnect, Utilities } = require('coralpay-c-connect-node-sdk');
-
 const { join } = require('path');
+
 
 const run = async () => {
   const passphrase = 'some-passphrase';
 
-  /*
+
+  console.log("\n==========================================\n");
+  console.log("Runing the CoralPayCConnect test...");
+  console.log("\n==========================================\n");
+
+
   const coral = new CoralPayCConnect({
     cConnectServiceBaseUrl: 'http://localhost.com/', 
-    cConnectPublicEncryptionKeyPath: 
-      join(__dirname,'./keys/coralpay.public_key.text'), 
-    privateKeyPath: join(__dirname, './keys/private_key.text'),
+    cConnectPublicEncryptionKeyPath: join(__dirname,'./keys/coralpay.public_key.txt'), 
+    privateKeyPath: join(__dirname, './keys/private_key.txt'),
     passphrase: 'some-passphrase',
-    userName: 'null',
+    username: 'null',
     password: 'null',
-    merchantId: 'null',
-    terminalId: 'null',
-    trace: false // enable this to see log of requests and responses or pass your custom logging function
+    merchant_id: 'null',
+    terminal_id: 'null',
+    trace: true // enable this to see log of requests and responses or pass your custom logging function
   });
-*/
 
+  const refRequest = await coral.invokeReference({ amount: 1200, channel: "WEB", trace_id: "1234567890" });
+
+
+  console.log("\n==========================================\n");
+  console.log("Finished runing the CoralPayCConnect test");
+  console.log("\n==========================================\n\n");
+
+
+
+
+  console.log("\n\n==========================================\n");
+  console.log("Runing the Utilities test...");
+  console.log("\n==========================================\n");
   const utilities = new Utilities({
-    publicEncryptionKeyPath: './keys/coralpay.public.key.txt',
+    publicEncryptionKeyPath: './keys/coralpay.public_key.txt',
      // join(__dirname,'./keys/coralpay.public_key.txt'), 
-    privateKeyPath: './keys/private.key.txt', // join(__dirname, './keys/private_key.txt'),
+    privateKeyPath: './keys/private_key.txt', // join(__dirname, './keys/private_key.txt'),
     passphrase: 'some-passphrase',
     trace: true // enable this to see log of requests and responses or pass your custom logging function
   });
@@ -31,6 +47,11 @@ const run = async () => {
   const payload = await utilities.encryptRequest({ ray: 'mond' });
   // const payload = await coral.encryptRequest({ ray: 'mond' });
   console.log('This is the Payload:\n\n' + payload, '\n');
+
+  console.log("\n==========================================\n");
+  console.log("Finished runing the Utilities test");
+  console.log("\n==========================================\n");
+
 };
 
 run();
